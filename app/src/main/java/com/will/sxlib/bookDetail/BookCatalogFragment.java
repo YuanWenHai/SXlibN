ffd41737ebda40324bc1601e4cfed6ed3f2216c7
YuanWenHai <a2265839@gmail.com>
2017-03-18 16:34:58 +0800

    继续完善了description fragment的界面

--- app/src/main/java/com/will/sxlib/bookDetail/BookCatalogFragment.java
+++ app/src/main/java/com/will/sxlib/bookDetail/BookCatalogFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 import com.will.sxlib.R;
 import com.will.sxlib.base.BaseFragment;
 
@@ -18,6 +19,11 @@ public class BookCatalogFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_book_detail_description,container,false);
+        View view = inflater.inflate(R.layout.fragment_book_detail_description,container,false);
+        ExpandableTextView summary = (ExpandableTextView) view.findViewById(R.id.fragment_book_catalog_summary);
+        ExpandableTextView catalog = (ExpandableTextView) view.findViewById(R.id.fragment_book_catalog_catalog);
+        summary.setText(getResources().getText(R.string.test_summary));
+        catalog.setText(getResources().getText(R.string.test_catalog));
+        return view;
     }
 }
